fd62cee523b3b9d005cabf32e3e8480191a28e30
Anderson Sousa <and3rsonsousa@outlook.com>
2024-06-14 16:39:05 -0300

    sem htc

--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -36,14 +36,14 @@ export default function Index() {
 					/>
 				</div>
 				<div className="flex my-12 flex-col gap-2">
-					<a
+					{/* <a
 						target="_blank"
 						rel="noreferrer"
 						href="https://wa.me/5588992270922?text=Ol%C3%A1,%20Gostaria%20de%20agendar%20uma%20CONSULTA%20DOMICILIAR%20com%20a%20Dra.%20Brenda."
 						className="link bg-yellow-100 focus:ring-yellow-300 hover:bg-yellow-200 text-yellow-700"
 					>
 						Agendar Consulta Domiciliar
-					</a>
+					</a> */}
 
 					<a
 						target="_blank"
@@ -61,14 +61,14 @@ export default function Index() {
 					>
 						Agendar Consulta na SmartMed
 					</a>
-					<a
+					{/* <a
 						target="_blank"
 						rel="noreferrer"
 						href="https://wa.me/5588998551739?text=Ol%C3%A1,%20Gostaria%20de%20agendar%20uma%20consulta%20com%20a%20Dra.%20Brenda."
 						className="link bg-lime-100 focus:ring-lime-300 hover:bg-lime-200 text-lime-700"
 					>
 						Agendar Consulta no HTC ( Ipu-CE )
-					</a>
+					</a> */}
 				</div>
 
 				<div className="text-xs text-slate-500 tracking-tight">
